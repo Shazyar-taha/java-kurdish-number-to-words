902084543a8ec257370dd340c325d7304177b80a
Shazyar <63422015+Shazyar-taha@users.noreply.github.com>
2021-09-26 20:25:18 +0300

    adding some comments

--- src/KurdishNumberToWords/NumberToKurdishWords.java
+++ src/KurdishNumberToWords/NumberToKurdishWords.java
@@ -2,23 +2,29 @@ package KurdishNumberToWords;
 
 import java.text.DecimalFormat;
 
+// convertion class
 public class NumberToKurdishWords {
-
+    // tens numbers
     private static final String[] tensNames = {"", " دە", " بیست", " سی", " چل", " پەنجا", " شەست", " حەفتا", " هەشتا", " نەوەد"};
-
+    // 0 - 19 names
     private static final String[] numNames = {"", " یەک", " دوو", " سێ", " چوار", " پێنج", " شەش", " حەوت", " هەشت", " نۆ", " دە", " یانزە",
         " دوانزە", " سیانزە", " چواردە", " پانزە", " شانزە", " حەڤە", " هەژدە", " نۆزدە"};
 
     private NumberToKurdishWords() {
     }
 
-    //this is the main work
+     /**
+    * number to kurdish words method
+    *
+    * @param number {long} - the convertion number
+    * @return {String} - the number kurdish words
+    */
     public static String convert(long number) {
         // 0 to 999 999 999 999
         if (number == 0) {
             return "سفر";
         }
-
+        // converting number to String
         String snumber = Long.toString(number);
 
         // pad with "0"
@@ -34,17 +40,20 @@ public class NumberToKurdishWords {
         int hundredThousands = Integer.parseInt(snumber.substring(6, 9));
         // nnnnnnnnnXXX
         int thousands = Integer.parseInt(snumber.substring(9, 12));
-
+        // billions string
         String tradBillions;
+        // if the number has billions, then convertion its part to words
         if (billions == 0) {
             tradBillions = "";
         } else {
             tradBillions = convertLessThanOneThousand(billions)
                     + setAnd(Integer.parseInt(snumber.substring(0, 12)), 1000000000, " ملیار");
         }
+        // result String
         String result = tradBillions;
-
+        // millions string
         String tradMillions;
+        // if the number has millions, then convertion its part to words
         if (millions == 0) {
             tradMillions = "";
         } else {
@@ -52,9 +61,9 @@ public class NumberToKurdishWords {
                     + setAnd(Integer.parseInt(snumber.substring(3, 12)), 1000000, " ملیۆن");
         }
         result = result + tradMillions;
-
+        // hundred thousands String
         String tradHundredThousands;
-
+        // if the number has hundred thousands, then convertion its part to words
         if (hundredThousands == 0) {
             tradHundredThousands = "";
         } else {
@@ -65,21 +74,30 @@ public class NumberToKurdishWords {
         result = result + tradHundredThousands;
 
         String tradThousand;
+        // thousands string, it takes the number's less than a thousand part
         tradThousand = convertLessThanOneThousand(thousands);
+        // generating result string
         result = result + tradThousand;
 
-        // remove extra spaces!
+        // remove extra spaces and returning the result
         return result.replaceAll("^\\s+", "").replaceAll("\\b\\s{2,}\\b", " ");
     }
 
+    /**
+    * less a thousand number to string
+    *
+    * @param number {int} - the convertion number
+    * @return {String} - the number kurdish words
+    */
     private static String convertLessThanOneThousand(int number) {
+        // less than a thousand number string
         String soFar;
-
+        // if 100's mod of number was between 0-19
         if (number % 100 < 20) {
             soFar = numNames[number % 100];
             number /= 100;
         } else {
-            //ama leraya bo away agar mody 10 krd krdia sfr nache (و) dabnet
+            // if 10's mod of the number was 0, it prevents adding "و"
             if (number % 10 == 0) {
 
                 number /= 10;
@@ -95,13 +113,22 @@ public class NumberToKurdishWords {
             }
 
         }
+        // if the number got to 0
         if (number == 0) {
             return soFar;
         }
+        // returning result
         return soFar.equals("") ? numNames[number] + " سەد" + soFar : numNames[number] + " سەد" + " و" + soFar;
     }
 
-    //bo danany (و) la newan zhmarakan
+    /**
+    * adding "و" between numbers
+    *
+    * @param number {long} - the convertion number
+    * @param number {long} - number's mod
+    * @param number {string} - number's word string
+    * @return {String} -result string
+    */
     private static String setAnd(long number, long mod, String text) {
         return (number % mod != 0) ? text + " و" : text;
     }
